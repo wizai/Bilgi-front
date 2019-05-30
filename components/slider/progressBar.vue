242db385446f2d3e30a14b3b07f62fd7f325a0d7
Romain <ferjuleromain@gmail.com>
2019-05-30 15:08:07 +0200

    [ DELETE COMPONENT / ADD COMPONENT PROGRESS / JS ] Delete Component slider

---
+++ components/slider/progressBar.vue
@@ -0,0 +1,62 @@
+<template>
+
+  <div class="slider-navbar">
+    <span>{{sliderCurrent}}</span>
+    <div class="slider-navbar__progressbar">
+      <div class="slider-navbar__progressbar--progress" ref="progressBar" :style="{ width: progress}"></div>
+    </div>
+    <span>{{sliderMax}}</span>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: "progressBar",
+    props: {
+      sliderCurrent: "",
+      sliderMax: "",
+      progress: ""
+    }
+  }
+</script>
+
+
+<style lang="scss">
+
+  @import "@/assets/scss/style.scss";
+
+  .slider-navbar {
+    position: absolute;
+    bottom: 100px;
+    left: 0;
+    width: 100%;
+    @include flexbox();
+    @include align-items(center);
+    @include justify-content(center);
+
+    span {
+      font-weight: 700;
+      margin: 0 15px;
+    }
+
+    &__progressbar {
+      position: relative;
+      width: 150px;
+      height: 1px;
+      background-color: #fff;
+
+      &--progress {
+        position: absolute;
+        top: -1px;
+        left: 0;
+        height: 3px;
+        width: 0%;
+        background-color: $blue;
+      }
+
+    }
+
+  }
+
+</style>
